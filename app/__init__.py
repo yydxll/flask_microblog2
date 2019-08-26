ee5a417a2bc537c499c52614e3d8eb19c14f7852
zhangsan <zhangsan@sina.com>
2019-08-26 15:05:12 +0800

    first

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import Config#从config模块导入Config类
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+
+
+from flask_migrate import Migrate
+from flask_login import LoginManager
+app = Flask(__name__)
+app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+login = LoginManager(app)
+from app import routes,models#导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
